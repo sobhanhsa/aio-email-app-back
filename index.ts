5f81b9d78ccc8e2d03c578086c42f9f0a67961f7
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-31 23:54:05 +0330

    styles:remove commented codes

--- index.ts
+++ index.ts
@@ -11,18 +11,8 @@ const app = express();
 import cookieParser from "cookie-parser";
 import router from "./userRouter";
 
-// const usercontrollers = require("./controllers/usercontroller");
-
 app.use(cookieParser());
 app.use(express.json());
 app.use("/",router)
-
-
-
-// app.post("/api/signup",usercontrollers.signupHandler);
-
-// app.post("/api/login",usercontrollers.loginHandler)
-
-// app.get("/api/logout",usercontrollers.logoutHandler)
 
 app.listen(port, () => console.log("server in running on port %d",port));
