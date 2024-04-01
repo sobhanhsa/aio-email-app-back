6b7cc3b239d794e6717b0165a3a23ce5232407db
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-01 22:52:51 +0330

    feat:add cors for express

--- index.ts
+++ index.ts
@@ -3,14 +3,19 @@ import { configDotenv } from "dotenv";
 configDotenv()
 
 import express from "express";
-
-const port = process.env.PORT || 8000;
-
-
 import cookieParser from "cookie-parser";
 import router from "./userRouter";
 import { app , server } from "./socket/socket";
+import cors from "cors";
 
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/",router)
