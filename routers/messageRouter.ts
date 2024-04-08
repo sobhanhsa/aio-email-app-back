f87661c5bec4a2f31c384321ad25733c8ea078e1
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-08 21:04:43 +0330

    feat : use correct handlers

--- routers/messageRouter.ts
+++ routers/messageRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import protectRoute from "../middlewares/authMiddleware";
-import { sendMessageHandler } from "../controllers/messageController";
+import { getMessagesHandler, sendMessageHandler } from "../controllers/messageController";
 
 const messageRouter = Router();
 
 messageRouter.post("/send/:id",protectRoute,sendMessageHandler)
-messageRouter.get("/:id",protectRoute,sendMessageHandler)
+messageRouter.get("/",protectRoute,getMessagesHandler)
 
 export default messageRouter
